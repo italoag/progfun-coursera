b31afbea7028f458b992076d057b4d9a9e03811e
Italo Gomes <italo@amorimgomes.com.br>
2013-09-24 01:53:47 -0300

    Exercices

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,6 @@
 package recfun
 import common._
+import scala.annotation.tailrec
 
 object Main {
   def main(args: Array[String]) {
@@ -14,15 +15,36 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
-
+//  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    def factorial(x: Int, accumulator: Int): Int = if (x == 0) accumulator else factorial(x-1, x*accumulator)
+    factorial(r, 1)/(factorial(r-c, 1)*factorial(c, 1))
+  }
+  
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
-
+  //def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+  @tailrec def balanced(chars: List[Char], count: Int): Boolean = 
+    chars match {
+      case Nil => count == 0
+      case '(' :: tail => balanced(tail, count + 1)
+      case ')' :: tail => count > 0 && balanced(tail, count - 1)
+      case  _  :: tail => balanced(tail, count)
+    }
+  	balanced(chars, 0)
+  }
+  
+  
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+//  def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+    if(money == 0) 1
+    else if (money < 0) 0
+    else if (coins.isEmpty) 0
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
+  }
 }
